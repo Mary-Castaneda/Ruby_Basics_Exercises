902a67253ec955716c639627633ac9d8ff0364c1
Mary-Castaneda <maryobray@gmail.com>
2023-08-11 03:39:11 +0000

    Ruby Basics Exercises: Hashes

---
+++ Hashes/labeled_numbers.rb
@@ -0,0 +1,17 @@
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+numbers.each {|key,value| puts "A #{key} number is #{value}."}
+
+half_numbers = numbers.map {|key,value| value / 2 }
+p half_numbers
+
+low_numbers = numbers.select { |key, value| value < 25 }
+p low_numbers
+
+low_numbers = numbers.select! { |key, value| value < 25 }
+p low_numbers
+p numbers

b6bbb3cc07839f65055633c006b81b43724c0a02
Mary-Castaneda <maryobray@gmail.com>
2023-08-11 01:21:07 +0000

    Ruby Basics Exercises: Arrays

---
+++ Arrays/pets.rb
@@ -0,0 +1,11 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+my_pets = pets[2..3]
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
+
+my_pets.pop
+puts "I have a pet #{my_pets[0]}!"
+
+my_pets.push(pets[1])
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
+
